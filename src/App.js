71f5a203b502c46b333da35bfff5f37ac3adad0c
Stephen Lyons <stephen.lyons18@gmail.com>
2020-06-30 21:59:29 -0700

    Components now pull information from the JSON file

--- src/App.js
+++ src/App.js
@@ -14,48 +14,54 @@ import { BsArrowLeft } from "react-icons/bs";
 
 
 function App() {
-  questions = require('./questions.json');
-  //const answer1 = questions[0].answer1;
-  const answer2 = "Test2";
+  let i = 0;
+  const questions = require('./questions.json');
+  let answer1Holder = questions[i].answer1.text;
+  let answer2Holder = questions[i].answer2.text;
+  let answer3Holder = questions[i].answer3.text;
+  let answer4Holder = questions[i].answer4.text;
+  let questionHolder = questions[i].questionText;
+  let imageHolder = questions[i].picture;
 
   return (
 
     <div className="App">
-      <div class="container-fluid">
-        <div class="jumbotron fluid-jumbotron">
+      <div className="container-fluid">
+        <div className="jumbotron fluid-jumbotron">
           <Image src="test.jpg/100px250" fluid/>
           <h6>This is where the image will go</h6>
         </div>
-        <div class="jumbotron fluid-jumbotron">
-          <h3>This is where the question will go</h3>
+        <div className="jumbotron fluid-jumbotron">
+          <h3>{questionHolder}</h3>
         </div>
-        <div id="button group">
+        <div className="container" id="button group">
           {/* Radio buttons for the answers to the questions */}
-          <ToggleButtonGroup type="radio" name="answers" defaultValue="{1}" vertical>
+          <ToggleButtonGroup type="radio" name="answers" vertical>
             
-            {/* <ToggleButton value={1} size="lg" id="answer1" variant="outline-primary" block>{answer1}</ToggleButton> */}
-            <ToggleButton value={2} size="lg" id="answer2" variant="outline-primary" block>{answer2}</ToggleButton>
-            <ToggleButton value={3} size="lg" id="answer3" variant="outline-primary" block>Answer 3</ToggleButton>
-            <ToggleButton value={4} size="lg" id="answer4" variant="outline-primary" block>Answer 4</ToggleButton>
+              <ToggleButton value={1} id="answer1" variant="outline-primary" block>{answer1Holder}</ToggleButton>
+              <ToggleButton value={2} id="answer2" variant="outline-primary" block>{answer2Holder}</ToggleButton>          
+              <ToggleButton value={3} id="answer3" variant="outline-primary" block>{answer3Holder}</ToggleButton>
+              <ToggleButton value={4} id="answer4" variant="outline-primary" block>{answer4Holder}</ToggleButton>
+            
           </ToggleButtonGroup>
           </div>
           
           {/* still need navigation buttons, but need to figure out JSON import issue first */}
           {/* Eventually these buttons will be put on the sides respectively */}
-          <div class="pull-left">
+          <div className="pull-left">
             <Button variant='outline-danger' id='nav-backward'><BsArrowLeft/></Button>
           </div>
-          <div class="pull-right">
-            <Button variant='outline-danger' id='nav-forward' class='pull-left'><BsArrowRight/></Button>
+          <div className="pull-right">
+            <Button variant='outline-danger' id='nav-forward' className='pull-left'  onClick={i++, answer1Holder = questions[i].answer1.Text}><BsArrowRight/></Button>
           </div>
-          <br/>
+         
           {/*This is the submit button that will apprear when the student has reached the end of the quiz and will check their answers and load form*/}
           <Button variant='outline-primary' id='check-answers'>Check Answers</Button>
-          <br/>
+        
 
           {/*This is the component that will be loaded at the end of the quiz*/}
           {/*This is a form element and will take the user's information*/}
-          <div class='container-sm' style={{
+          <div className='container-sm' style={{
             paddingLeft: 250,
             paddingRight:250
           }}>
