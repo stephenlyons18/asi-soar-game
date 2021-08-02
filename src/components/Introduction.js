5be94c6c77b0778e802bd389b337ae450b966e86
stephenlyons18 <stephen.lyons18@gmail.com>
2021-08-02 13:29:42 -0700

    new build july

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -59,6 +59,7 @@ const Introduction = () => {
                                         <iframe
                                             width="100%"
                                             height="400"
+                                            title="Soar Game Introduction"
                                             src="https://www.youtube.com/embed/6cftoxb0uOM"
                                             frameBorder="0"
                                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
